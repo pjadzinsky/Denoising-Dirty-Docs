6924be3dc726285c82ddeedfc5465f5a3413a86e
Pablo Jadzinsky <pjadzinsky@gmail.com>
2015-07-18 17:37:09 -0700

    Workgin on SavePredictions

--- code/architecture_1.py
+++ code/architecture_1.py
@@ -1,6 +1,6 @@
 from keras.models import Sequential, Graph
 from keras.layers.core import Dense, Activation, Merge, Reshape, Flatten
-from keras.layers.convolutional import Convolution2D, CropImage
+from keras.layers.convolutional import Convolution2D
 from keras.optimizers import SGD
 from keras.callbacks import ModelCheckpoint, Callback
 from code.load_data import load_data
@@ -8,38 +8,26 @@ import numpy as np
 import pdb
 
 class model(object):
-    def __init__(self, weights_file=None, flatten=False):
-        # if flatten is set, training and test data has been flattened
-        # reshape it into 248x540
-        nb_filters = [1]
+    def __init__(self, weights_file=None, nb_filters=1):
         graph = Graph()
 
-        # independently of flatten, what comes out is 'input' as a 4D tensor
-        if flatten:
-            graph.add_input(name='flatten_input', ndim=2)
-            graph.add_node(Reshape(1, 248, 540), name='input',
-                    input='flatten_input')
-        else:
-            graph.add_input(name='input', ndim=4)
+        graph.add_input(name='input', ndim=4)
 
-        graph.add_node(Convolution2D(nb_filters[0], 1, 3, 3, border_mode='same'),
+        graph.add_node(Convolution2D(nb_filters, 1, 3, 3, border_mode='same'),
                 name='scores_1a', input='input')
-        graph.add_node(Activation('sigmoid'), name='activations_1a', input='scores_1a')
-        
-        #graph.add_node(Convolution2D(nb_filters[1], 1, 1, 1, border_mode='valid'),
-        #        name='scores_1b', input='input')
-        #graph.add_node(Activation('relu'), name='activations_1b', input='scores_1b')
 
-        graph.add_output(name='output', input='activations_1a')
-        # Merge the two pathways
-        #graph.add_node(Dense(
-        #model = Sequential()
-        #model.add(Merge([graph, graph], mode='concat', axis=1))
-        #model.add(Convolution2D(1, sum(nb_filters), 1, 1))
-        #model.add(Activation('sigmoid'))
+        graph.add_node(Convolution2D(nb_filters, 1, 1, 1, border_mode='valid'),
+                name='scores_1b', input='input')
 
-        if flatten:
-            graph.add_node(Flatten())
+        graph.add_node(Activation('relu'), name='activations_1', inputs=['scores_1a', 'scores_1b'], merge_mode='sum')
+
+        graph.add_node(Convolution2D(1, nb_filters, 3, 3, border_mode='same'), 
+                name='scores_2', input='activations_1')
+
+        graph.add_node(Activation('sigmoid'),
+                name='activations_2', input='scores_2')
+
+        graph.add_output(name='output', input='activations_2')
 
         if weights_file is not None:
             graph.load_weights(weights_file)
@@ -57,25 +45,11 @@ class model(object):
         #savemodels = SaveModels()
         #history = LossHistory()
         checkpointer = MyModelCheckpoint(filepath='temp.hdf5', verbose=1, save_best_only=False)
-        self.graph.fit({'input':X, 'output':Y}, nb_epoch=nb_epoch, batch_size=32, verbose=1, callbacks=[checkpointer])
+        checkpred=SavePredictions(filepath="test_batch.hdf5")
+        self.graph.fit({'input':X, 'output':Y}, nb_epoch=nb_epoch, batch_size=32, verbose=1, callbacks=[checkpointer, checkpred])
         #self.model.fit(X, Y, nb_epoch=nb_epoch, batch_size=32, show_accuracy=True, verbose=1, callbacks=[checkpointer])
         predictions = self.graph.predict({'input':X})
         return predictions
-
-    def predict(self, X, binary_flag=False):
-        prediction = self.model.predict(X)
-        
-        if binary_flag:
-            # for each image, compute a threshold
-            temp = prediction.reshape(prediction.shape[0], -1)
-            threshold = ( temp.max(axis=1) + temp.min(axis=1) )/2
-            threshold = threshold.reshape(-1, 1, 1, 1)
-
-            prediction = np.where(prediction > threshold, np.ones_like(prediction), np.zeros_like(prediction))
-
-        prediction = 1 - prediction
-
-        return prediction
 
 #class SaveModels(Callback):
 class MyModelCheckpoint(ModelCheckpoint):
@@ -83,10 +57,48 @@ class MyModelCheckpoint(ModelCheckpoint):
     def on_epoch_end(self, epoch, logs={}):
         self.filepath = 'weights_' + str(epoch) + '.hdf5'
         super(MyModelCheckpoint, self).on_epoch_end(epoch, logs)
-
-"""
+        
 class SavePredictions(Callback):
-    def on_epoch_begin(self, epoch, logs-{}):
-        X = 
-        self.model.predict
-"""
+    def __init__(self, filepath, verbose=0):
+        super(Callback, self).__init__()
+        
+        self.verbose = verbose
+        self.filepath = filepath
+
+    def on_epoch_begin(self, epoch, logs={}):
+        print(self.__dict__)
+        print(self.model.__dict__)
+        X = self.model.batch
+        predictions = self.model.predict(X)
+
+        filepath = self.filepath.format(str(epoch))
+        
+        # Save predictions to HDF5
+        import h5py
+
+        if epoch==0:
+            import os.path
+
+            # if file exists and should not be overwritten
+            if not overwrite and os.path.isfile(filepath):
+                import sys
+                get_input = input
+                if sys.version_info[:2] <= (2, 7):
+                    get_input = raw_input
+                overwrite = get_input('[WARNING] %s already exists - overwrite? [y/n]' % (filepath))
+                while overwrite not in ['y', 'n']:
+                    overwrite = get_input('Enter "y" (overwrite) or "n" (cancel).')
+                if overwrite == 'n':
+                    return
+                print('[TIP] Next time specify overwrite=True in save_weights!')
+
+                f = h5py.File(filepath, 'w')
+        else:
+            f = h5py.File(filepath, 'a')
+
+            g = f.create_group(str(epoch))
+            dset = g.create_dataset('predictions', predictions.shape, dtype=predictions.dtype)
+            dset[:] = predictions
+        
+        f.flush()
+        f.close()
