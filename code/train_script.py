5aa0d638adc47da80b82c68386bad93a67b005a6
Pablo D. Jadzinsky <jadz@stanford.edu>
2015-07-24 15:25:05 -0700

    Changes to train_script.py

--- code/train_script.py
+++ code/train_script.py
@@ -1,12 +1,11 @@
 import utils, architectures, load_data
 
-train, train_cleaned, test = load_data.load_data()
+train, train_cleaned= load_data.load_data(3)
 
-nb_filters = 1
-epochs = 1000
-save_every_epochs = 100
-nb_model = 4
+nb_filters = 10
+epochs = 101
+save_every_epochs = 10
+nb_model = 6
 model2 = architectures.model(nb_model, nb_filters=nb_filters)
-model2.fit(train, train_cleaned, epochs, save_models=list(range(0,epochs,save_every_epochs))+[epochs-1])
+model2.fit(train, train_cleaned, epochs, save_models=list(range(0,epochs,save_every_epochs)))
 
-architectures.savePredictions(nb_model, nb_filters, train[16,0,:,:])
